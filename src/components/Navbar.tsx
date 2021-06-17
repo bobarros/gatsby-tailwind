34ea25a5a26df2a7fa996f8120758e4d8f41d679
Bruno Barros <bobarros@gmail.com>
2021-06-17 11:17:11 -0300

    navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,22 +2,42 @@
 
 import React from "react";
 
-////////////////////////////////////////// Gatsby
+///////////////////////////////////////////// Gatsby
 
 import { Link } from "gatsby";
+
+/////////////////////////////////// Local Components
+
+import { LogoSVG } from "../components";
+
+////////////////////////////////////// Third Parties
+
+import { FiAlignJustify } from "react-icons/fi";
 
 ////////////////////////////////////////// Component
 
 const Navbar = () => {
   return (
-    <nav>
-      <ul>
-        <li><Link to="/">home</Link></li>
-        <li><Link to="/about">about</Link></li>
-        <li><Link to="/contact">contact</Link></li>
-        <li><Link to="/tags">tags</Link></li>
-        <li><Link to="/recipes">recipes</Link></li>
-      </ul>
+    <nav className="flex items-center justify-center lg:h-24">
+      <div className="w-90w max-w-maxvar lg:flex lg:items-center">
+        <div className="h-24 flex justify-between items-center lg:p-0 lg:mr-o lg:h-auto">
+          <Link to="/">
+            <LogoSVG />
+          </Link>
+          <button className="nav-btn">
+            <FiAlignJustify />
+          </button>
+        </div>
+        <div className="nav-links show-links">
+          <Link to="/" className="nav-link" activeClassName="active-link">home</Link>
+          <Link to="/about" className="nav-link" activeClassName="active-link">about</Link>          
+          <Link to="/tags" className="nav-link" activeClassName="active-link">tags</Link>
+          <Link to="/recipes" className="nav-link" activeClassName="active-link">recipes</Link>
+        </div>
+        <div className="nav-link contact-link">
+          <Link to="/contact" className="btn">contact</Link>
+        </div>
+      </div>
     </nav>
   );
 };
--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,7 +1,9 @@
 import NavBar from './Navbar';
 import Footer from './Footer';
+import LogoSVG from './LogoSVG';
 
 export { 
   NavBar,
   Footer,
+  LogoSVG,
  };
