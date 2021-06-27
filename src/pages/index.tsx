e1e3e41e7da4e1e17aa993247b846cf6b9a686f1
Bruno Barros <bobarros@gmail.com>
2021-06-27 16:46:41 -0300

    index home hero

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,15 +2,33 @@
 
 import React from "react";
 
+///////////////////////////////////////////// Gatsby
+
+import { StaticImage } from "gatsby-plugin-image"
+
 ////////////////////////////////////////// Components
 
-import { Layout } from '../layouts'
+import { Layout } from "../layouts";
 
 const Index = () => {
   return (
     <Layout>
       <div className={div}>
-        Home page
+        <header className={hero}>
+          <StaticImage
+            src="../images/main.jpeg"
+            alt="eggs"
+            className={image}
+            placeholder="tracedSVG"
+            layout="fullWidth"
+          />
+          <div className={container}>
+            <div className={text}>
+              <h1 className={header}>simply recipes</h1>
+              <h4>no fluff, just recipes</h4>
+            </div>
+          </div>
+        </header>
       </div>
     </Layout>
   );
@@ -21,4 +39,9 @@ export default Index;
 /////////////////////////////////////////// Tailwind style
 
 const 
-  div = "p-6 max-w-sm mx-auto bg-white rounded-xl shadow-md flex items-center space-x-4";
+  div = "w-90w max-w-maxvar min-h-minvar my-0 mx-auto",
+  hero = "h-40h relative mb-8",
+  image = "h-full rounded-radius",
+  container = "absolute top-0 left-0 w-full h-full flex items-center justify-center bg-black bg-opacity-40 rounded-radius",
+  text = "text-white text-center",
+  header = "md:text-6xl md:mb-0";
